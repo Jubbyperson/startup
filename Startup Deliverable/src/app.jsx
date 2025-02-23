58fb0d48bd9023c8df0b5d83333f1bd06ae3d4d4
Jackson <jjsteph@byu.edu>
2025-02-23 14:47:43 -0700

    added routes

--- Startup Deliverable/src/app.jsx
+++ Startup Deliverable/src/app.jsx
@@ -9,27 +9,36 @@ import { Social } from './social/social';
 import { Workouts } from './workouts/workouts';
 
 export default function App() {
-  return <div className="body bg-dark text-light">
-    <header>
-        <nav>
-            <menu>
-                <li class="title"><img src="https://t4.ftcdn.net/jpg/02/43/26/65/360_F_243266561_JPLUStDWwsBJ05qOZsWK3G1ARB6pU8Se.jpg" width="150" length="100"></img>GymShare</li>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="workouts.html">Workouts</a></li>
-                <li><a href="mealPlans.html">Meal Plans</a></li>
-                <li><a href="social.html">Social</a></li>
-            </menu>
-        </nav>
-        <hr />
-        </header>
+  return (
+    <BrowserRouter>
+        <div className="body bg-dark text-light">
+            <header>
+                <nav>
+                    <menu>
+                        <li class="title"><img src="https://t4.ftcdn.net/jpg/02/43/26/65/360_F_243266561_JPLUStDWwsBJ05qOZsWK3G1ARB6pU8Se.jpg" width="150" length="100"></img>GymShare</li>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="workouts.html">Workouts</a></li>
+                        <li><a href="mealPlans.html">Meal Plans</a></li>
+                        <li><a href="social.html">Social</a></li>
+                    </menu>
+                </nav>
+                <hr />
+                </header>
 
-        <main>
-            App Components go Here
-        </main>
-        <footer>
-        <hr />
-        <span class="text-reset">Jackson Stephens</span>
-        <br/>
-        <a href="https://github.com/Jubbyperson/startup-deliverable.git">GitHub Repository</a>
-        </footer></div>;
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/workouts" element={<Workouts />} />
+                    <Route path="/mealPlans" element={<MealPlans />} />
+                    <Route path="/social" element={<Social />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+
+                <footer>
+                <hr />
+                <span class="text-reset">Jackson Stephens</span>
+                <br/>
+                <a href="https://github.com/Jubbyperson/startup-deliverable.git">GitHub Repository</a>
+                </footer></div>;
+    </BrowserRouter>
+    );
 }
