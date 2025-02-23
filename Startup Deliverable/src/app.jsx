102ece3b746a379946034094ab725295f848f3ec
Jackson <jjsteph@byu.edu>
2025-02-23 15:04:25 -0700

    Added notfound function

--- Startup Deliverable/src/app.jsx
+++ Startup Deliverable/src/app.jsx
@@ -9,19 +9,19 @@ import { Social } from './social/social';
 import { Workouts } from './workouts/workouts';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-        <div className="body bg-dark text-light">
-            <header>
-                <nav>
-                    <menu>
-                        <li class="title"><img src="https://t4.ftcdn.net/jpg/02/43/26/65/360_F_243266561_JPLUStDWwsBJ05qOZsWK3G1ARB6pU8Se.jpg" width="150" length="100"></img>GymShare</li>
-                        <li><NavLink to="index">Home</NavLink></li>
-                        <li><NavLink to="workouts">Workouts</NavLink></li>
-                        <li><NavLink to="mealPlans">Meal Plans</NavLink></li>
-                        <li><NavLink to="social">Social</NavLink></li>
-                    </menu>
-                </nav>
+    return (
+        <BrowserRouter>
+            <div className="body bg-dark text-light">
+                <header>
+                    <nav>
+                        <menu>
+                            <li class="title"><img src="https://t4.ftcdn.net/jpg/02/43/26/65/360_F_243266561_JPLUStDWwsBJ05qOZsWK3G1ARB6pU8Se.jpg" width="150" length="100"></img>GymShare</li>
+                            <li><NavLink to="index">Home</NavLink></li>
+                            <li><NavLink to="workouts">Workouts</NavLink></li>
+                            <li><NavLink to="mealPlans">Meal Plans</NavLink></li>
+                            <li><NavLink to="social">Social</NavLink></li>
+                        </menu>
+                    </nav>
                 <hr />
                 </header>
 
@@ -35,10 +35,14 @@ export default function App() {
 
                 <footer>
                 <hr />
-                <span class="text-reset">Jackson Stephens</span>
-                <br/>
-                <a href="https://github.com/Jubbyperson/startup-deliverable.git">GitHub Repository</a>
+                    <span class="text-reset">Jackson Stephens</span>
+                    <br/>
+                    <a href="https://github.com/Jubbyperson/startup-deliverable.git">GitHub Repository</a>
                 </footer></div>;
-    </BrowserRouter>
+        </BrowserRouter>
     );
 }
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
