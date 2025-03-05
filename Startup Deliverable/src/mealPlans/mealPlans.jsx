7358262e0ec19f56824bc533b70816eb0685d085
Jackson <jjsteph@byu.edu>
2025-03-04 19:14:30 -0700

    Continued work on mealplans.jsx

--- Startup Deliverable/src/mealPlans/mealPlans.jsx
+++ Startup Deliverable/src/mealPlans/mealPlans.jsx
@@ -84,46 +84,27 @@ export function MealPlans() {
             </select>
             <br/>
             <button type="submit" onClick={handleSearch}>Search</button>
-            <h2>High Protein</h2>
-            <p><i>content here</i></p>
-            <input type = "text" placeholder="post a meal here!"/>
-            <input type = "text" placeholder="Name your new meal here!"/>
-            <button type="submit">Post</button>
-            <h2>High Carb</h2>
-            <p><i>content here</i></p>
-            <input type = "text" placeholder="post a meal here!"/>
-            <input type = "text" placeholder="Name your new meal here!"/>
-            <button type="submit">Post</button>
-            <h2>Low Carb</h2>
-            <p><i>content here</i></p>
-            <input type = "text" placeholder="post a meal here!"/>
-            <input type = "text" placeholder="Name your new meal here!"/>
-            <button type="submit">Post</button>
-            <h2>High Fat</h2>
-            <p><i>content here</i></p>
-            <input type = "text" placeholder="post a meal here!"/>
-            <input type = "text" placeholder="Name your new meal here!"/>
-            <button type="submit">Post</button>
-            <h2>Low Fat</h2>
-            <p><i>content here</i></p>
-            <input type = "text" placeholder="post a meal here!"/>
-            <input type = "text" placeholder="Name your new meal here!"/>
-            <button type="submit">Post</button>
-            <h2>Calorie Dense</h2>
-            <p><i>content here</i></p>
-            <input type = "text" placeholder="post a meal here!"/>
-            <input type = "text" placeholder="Name your new meal here!"/>
-            <button type="submit">Post</button>
-            <h2>Low Calorie</h2>
-            <p><i>content here</i></p>
-            <input type = "text" placeholder="post a meal here!"/>
-            <input type = "text" placeholder="Name your new meal here!"/>
-            <button type="submit">Post</button>
-            <h2>Tasty Cheat Day Meals!</h2>
-            <p><i>content here</i></p>
-            <input type = "text" placeholder="post a meal here!"/>
-            <input type = "text" placeholder="Name your new meal here!"/>
-            <button type="submit">Post</button>
-        </main>
+            {Object.keys(meals).map((category) => (
+        <div key={category}>
+          <h2>{category}</h2>
+          {meals[category].map((meal, index) => (
+            <p key={index}><i>{meal.name}: {meal.content}</i></p>
+          ))}
+          <input
+            type="text"
+            placeholder="post a meal here!"
+            value={newMealInputs[category].meal}
+            onChange={(e) => handleInputChange(category, 'meal', e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Name your new meal here!"
+            value={newMealInputs[category].name}
+            onChange={(e) => handleInputChange(category, 'name', e.target.value)}
+          />
+          <button type="button" onClick={() => handlePost(category)}>Post</button>
+        </div>
+      ))}
+    </main>
   );
 }
