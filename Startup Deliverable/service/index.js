6bd005ad15cc586229d9b789bb1fdf1df936dabe
Jackson <jjsteph@byu.edu>
2025-03-31 23:51:16 -0600

    working on backend index.js file

--- Startup Deliverable/service/index.js
+++ Startup Deliverable/service/index.js
@@ -104,3 +104,11 @@ apiRouter.post('/mealPlans', verifyAuth, (req, res) => {
   mealPlans.push(req.body);
   res.send(mealPlans);
 });
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
