69813fd9ac12f8ff81ab3c029277d4884b8fdb6e
Jackson <jjsteph@byu.edu>
2025-03-31 23:37:46 -0600

    working on backend index.js file

---
+++ Startup Deliverable/service/index.js
@@ -0,0 +1,7 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
