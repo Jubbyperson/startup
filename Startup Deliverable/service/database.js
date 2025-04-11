7c2da3607e0f6304631daa6ea477267f88d807f1
Jackson <jjsteph@byu.edu>
2025-04-10 19:32:02 -0600

    working on database.js

--- Startup Deliverable/service/database.js
+++ Startup Deliverable/service/database.js
@@ -1,2 +1,11 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('gymshare');
+const userCollection = db.collection('user');
+const workoutCollection = db.collection('workout');
+const socialCollection = db.collection('social');
+const mealPlanCollection = db.collection('mealPlan');
+
