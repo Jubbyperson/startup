21f4e6ad9e0332893b8e8d470c62c61bb6925c75
Jackson <jjsteph@byu.edu>
2025-04-15 16:25:42 -0600

    Working on websocket implementation

--- Startup Deliverable/service/peerProxy.js
+++ Startup Deliverable/service/peerProxy.js
@@ -1,2 +1,7 @@
 const { WebSocketServer } = require('ws');
 
+function peerProxy(httpServer) {
+  const socketServer = new WebSocketServer({ server: httpServer });
+
+  socketServer.on('connection', (socket) => {
+    socket.isAlive = true;
